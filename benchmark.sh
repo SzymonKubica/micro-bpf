192d90564c84f50ec694e1be2a151149e0e8f82b
SzymonKubica <szymo.kubica@gmail.com>
2024-02-08 21:32:45 +0100

    Updated the adjust-bytecode and benchmark scripts.

--- benchmark.sh
+++ benchmark.sh
@@ -1,6 +1,6 @@
 for chunks in {0..7}
 do
- aiocoap-client -m POST 'coap://[fe80::a8e8:48ff:fee0:523c%enp34s0u2u1u2]/benchmark' --payload "$chunks" | tee -a results.txt
+ aiocoap-client -m POST 'coap://[fe80::a8e8:48ff:fee0:523c%wlan0]/benchmark' --payload "$chunks" | tee -a results.txt
  # Add a newline
  echo "" >> results.txt
 done

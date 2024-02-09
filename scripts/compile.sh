4c222e70dafb17f89111d1ea780cda4fd5d8004d
SzymonKubica <szymo.kubica@gmail.com>
2024-02-09 22:34:05 +0100

    Updated the compile script to allow warnings.

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,1 +1,1 @@
-clang -fno-stack-protector -O2 -emit-llvm -c $1 -o - | llc -march=bpf -filetype=obj -o $2
+CFLAGS="-W-no-error" clang -fno-stack-protector -O2 -emit-llvm -c $1 -o - | llc -march=bpf -filetype=obj -o $2

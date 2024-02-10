845f2f87cbe719af28f934785d401db93f5f6bac
SzymonKubica <szymo.kubica@gmail.com>
2024-02-10 14:29:59 +0100

    Added the test for saul-reg helpers demonstrating full FC vm replacement.

---
+++ bpf/helper-tests/femto-container/saul-reg.c
@@ -0,0 +1,49 @@
+#include <stdint.h>
+#include "bpf/bpfapi/helpers.h"
+
+/* This file tests whether the saul_reg helper functions work correctly.
+ * It assumes that the VM is running on a RIOT instance which has these modules
+ * loaded: saul, saul_reg, saul_default. It also assumes that it is running on
+ * an stm32 (board name: nucleo-f439zi) and thus the default SAUL configuration
+ * has access to the three on-board leds and the user button switch.
+ */
+
+const int SAUL_ACT_SWITCH = 68;
+const int SAUL_SENSE_BTN = 129;
+
+int test_saul_reg_find(void *ctx)
+{
+    (void)ctx;
+
+    bpf_saul_reg_t *diode0;
+    bpf_saul_reg_t *diode1;
+    bpf_saul_reg_t *diode2;
+    phydat_t diode_payload;
+
+    // First get pointers to the device drivers for the on-board LEDS.
+    diode0 = bpf_saul_reg_find_nth(0);
+    diode1 = bpf_saul_reg_find_nth(1);
+    diode2 = bpf_saul_reg_find_nth(2);
+
+    // Payload tells diodes to turn on.
+    diode_payload.val[0] = 1;
+
+    bpf_saul_reg_write(diode0, &diode_payload);
+    bpf_saul_reg_write(diode1, &diode_payload);
+    bpf_saul_reg_write(diode2, &diode_payload);
+
+    // Now we want to test finding SAUL devices by their type.
+    bpf_saul_reg_t *user_button;
+    user_button = bpf_saul_reg_find_type(SAUL_SENSE_BTN);
+    phydat_t button_state;
+
+    // Read the state of the button.
+    // Normally it should write 0 into val[0] of the button state.
+    // If this script is executed while the button is being held down,
+    // it should write 1 into val[0].
+    bpf_saul_reg_read(user_button, &button_state);
+
+    print("Button state: %d\n", button_state.val[0]);
+
+    return 0;
+}

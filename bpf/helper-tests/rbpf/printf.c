bde4676d771e461929301d7c1fd1f0070cd6c906
SzymonKubica <szymo.kubica@gmail.com>
2024-02-10 12:14:20 +0100

    Added the new helpers-tests directory for bpf snippets ensuring that rbpf is a drop-in replacement for FC vm.

---
+++ bpf/helper-tests/rbpf/printf.c
@@ -0,0 +1,20 @@
+#include <linux/bpf.h>
+#include <stdint.h>
+#include <string.h>
+#include <bpf/bpf_helpers.h>
+#include "../../helpers.h"
+
+
+SEC(".main")
+int test_printf(void *ctx)
+{
+    // Here we use the macro to avoid defining the format string explicitly
+    print("printf accepts up to 4 args: %d %d %d %d\n", 1, 2, 3, 4);
+
+    // We can also use the helper directly, however in that case we need to
+    // first declare the char[]
+    char fmt[] = "printf accepts up to 4 args: %d %d %d %d\n";
+    bpf_printf(fmt, 5, 6, 7, 8);
+    return 0;
+}
+

c8bae03d416702e4b42d35e05e40a0158ad4e685
SzymonKubica <szymo.kubica@gmail.com>
2024-02-25 09:30:04 +0000

    Tested the gpio access using the new fc-like binary format. Also added wrapper scripts to easier edit the tools commands.

---
+++ bpf/helper-tests/gpio.c
@@ -0,0 +1,36 @@
+#include <stdint.h>
+#include <linux/bpf.h>
+#include "../helpers.h"
+#include <bpf/bpf_helpers.h>
+
+int gpio_write(void *ctx)
+{
+    (void)ctx;
+
+    // PA7 corresponds to D12 to which LED is connected
+    // Port A
+    uint32_t port_a = 0;
+    // Pin 7
+    uint32_t pin = 6;
+
+    // PF13 corresponds to D7 where the microphone send digital output
+    // Port F
+    uint32_t port_f = 5;
+    // Pin 13
+    uint32_t pin_2 = 13;
+
+    // We read the microphone value until sound is detected.
+    // After this we turn on the LED and terminate
+    uint64_t mic_value = 0;
+    while (!mic_value) {
+        mic_value = bpf_gpio_read_input(port_f, pin_2);
+    }
+
+    // Toggle the led
+    uint32_t value = 128;
+    if (bpf_gpio_read_raw(port_a, pin)) {
+        value = 0;
+    }
+    bpf_gpio_write(port_a, pin, value);
+    return (int) value;
+}
---
+++ execute.sh
@@ -0,0 +1,1 @@
+./tools/target/release/mibpf-tools execute --riot-ipv6-addr fe80::a8e8:48ff:fee0:523c --suit-storage-slot $1 --host-network-interface enp82s0u2u1u2 --target $2

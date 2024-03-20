4bca76dc15296752f5962c06b55616ba607f0cd6
SzymonKubica <szymo.kubica@gmail.com>
2024-03-20 21:48:30 +0000

    Renamed the vm thread module and changed the order of processing requests in the manager to avoid starvation.

--- mibpf-server/src/vm/mod.rs
+++ mibpf-server/src/vm/mod.rs
@@ -1,11 +1,11 @@
 
 mod vm;
 pub mod rbpf_vm;
-mod vm_thread;
+mod vm_manager;
 mod femtocontainer_vm;
 pub mod middleware;
 pub use vm::VirtualMachine;
 pub use rbpf_vm::RbpfVm;
 pub use femtocontainer_vm::FemtoContainerVm;
-pub use vm_thread::VMExecutionManager;
-pub use vm_thread::VM_EXEC_REQUEST;
+pub use vm_manager::VMExecutionManager;
+pub use vm_manager::VM_EXEC_REQUEST;

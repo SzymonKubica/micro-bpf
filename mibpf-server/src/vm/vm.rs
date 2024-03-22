512f8841f8507037e9b37979c377d3b44bb93764
SzymonKubica <szymo.kubica@gmail.com>
2024-03-22 23:51:12 +0000

    Started refactoring the execute on CoAP endpoints.

--- mibpf-server/src/vm/vm.rs
+++ mibpf-server/src/vm/vm.rs
@@ -11,7 +11,10 @@ pub trait VirtualMachine {
     fn execute(&self, program: &[u8], result: &mut i64) -> u32;
 
     /// Executes a given eBPF program giving it access to the provided PacketBuffer
-    /// and stores the return value of the program in `result`. It returns the VM execution time
+    /// and stores the return value of the program in `result`. The value returned
+    /// by the program and written to `result` needs to represent the length of
+    /// the packet PDU + payload. The reason for this is that the handler then
+    /// needs to know this length when sending the response back
     fn execute_on_coap_pkt(&self, program: &[u8], pkt: &mut PacketBuffer, result: &mut i64) -> u32;
 }
 

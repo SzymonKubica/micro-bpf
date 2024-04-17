a73409f77a291faf42069c69aa947cdb6605f152
SzymonKubica <szymo.kubica@gmail.com>
2024-04-17 14:23:03 +0200

    Updated the vm wrapper to specify the allowed buffer region for the coap handler.

--- mibpf-server/src/vm/rbpf_vm.rs
+++ mibpf-server/src/vm/rbpf_vm.rs
@@ -78,29 +78,38 @@ impl VirtualMachine for RbpfVm<'_> {
         middleware::helpers::register_helpers(&mut self.vm, self.registered_helpers.clone());
 
         let (ret, execution_time) =
-            self.timed_execution(|| self.vm.execute_program(&alloc::vec![], &alloc::vec![]));
+            self.timed_execution(|| self.vm.execute_program(&alloc::vec![], &alloc::vec![], alloc::vec![]));
         *result = ret;
         execution_time
     }
     fn execute_on_coap_pkt(&mut self, pkt: &mut PacketBuffer, result: &mut i64) -> u32 {
         middleware::helpers::register_helpers(&mut self.vm, self.registered_helpers.clone());
 
-        let buffer: &mut [u8] = unsafe {
+        /// Coap context struct containing information about the buffer,
+        /// packet and its length. It is passed into the VM as the main buffer
+        /// on which the program operates.
+        let coap_context: &mut [u8] = unsafe {
+            const CONTEXT_SIZE: usize = core::mem::size_of::<CoapContext>();
             let ctx = pkt as *mut _ as *mut CoapContext;
             println!("Context: {:?}", *ctx);
-            from_raw_parts_mut(ctx as *mut u8, 32)
+            from_raw_parts_mut(ctx as *mut u8, CONTEXT_SIZE)
         };
 
-        let buffer_mutex = Mutex::new(buffer);
+        let buffer_mutex = Mutex::new(coap_context);
 
-        // Actual packet data, it is pointed to by the header of the packet buffer
+        // Actual packet struct
         let mem = unsafe {
             let ctx = pkt as *mut _ as *mut CoapContext;
             println!("Coap context: {:?}", *ctx);
             from_raw_parts_mut((*ctx).pkt as *mut u8, (*ctx).len)
         };
-        //let mem: &mut [u8] = &mut [0; 512];
+
         let mem_mutex = Mutex::new(mem);
+
+        let pkt_buffer_region: (u64, u64) = unsafe {
+            let ctx = pkt as *mut _ as *mut CoapContext;
+            ((*ctx).buf as *const u8 as u64, (*ctx).len as u64)
+        };
 
         // Here we need to do some hacking with locks as closures don't like
         // capturing &mut references from environment. It does make sense.
@@ -108,6 +117,7 @@ impl VirtualMachine for RbpfVm<'_> {
             self.vm.execute_program(
                 mem_mutex.lock().deref_mut(),
                 buffer_mutex.lock().deref_mut(),
+                alloc::vec![pkt_buffer_region]
             )
         });
         *result = ret;

68b04fdee5744f5b22fd5c3631446f60e04ac40d
SzymonKubica <szymo.kubica@gmail.com>
2024-05-11 14:45:40 +0100

    Changed the timed vm to ensure that the smalles possible subsets of the code are getting timed.

--- mibpf-server/src/vm/timed_vm.rs
+++ mibpf-server/src/vm/timed_vm.rs
@@ -35,35 +35,45 @@ impl<'a> VirtualMachine<'a> for TimedVm<'a> {
     fn resolve_relocations(&mut self, program: &'a mut [u8]) -> Result<&'a [u8], String> {
         let start = self.time_now();
         let result = self.vm.resolve_relocations(program);
-        self.results.borrow_mut().relocation_resolution_time = self.time_now() - start;
+        let end = self.time_now();
+
+        self.results.borrow_mut().relocation_resolution_time = end - start;
         return result;
     }
 
     fn verify(&self) -> Result<(), String> {
         let start = self.time_now();
         let result = self.vm.verify();
-        self.results.borrow_mut().verification_time = self.time_now() - start;
+        let end = self.time_now();
+
+        self.results.borrow_mut().verification_time = end - start;
         return result;
     }
 
     fn initialise_vm(&mut self, program: &'a [u8]) -> Result<(), String> {
         let start = self.time_now();
         let result = self.vm.initialise_vm(program);
-        self.results.borrow_mut().load_time = self.time_now() - start;
+        let end = self.time_now();
+
+        self.results.borrow_mut().load_time = end - start;
         return result;
     }
 
     fn execute(&mut self) -> Result<u64, String> {
         let start = self.time_now();
         let result = self.vm.execute();
-        self.results.borrow_mut().execution_time = self.time_now() - start;
+        let end = self.time_now();
+
+        self.results.borrow_mut().execution_time = end - start;
         return result;
     }
 
     fn execute_on_coap_pkt(&mut self, pkt: &mut PacketBuffer) -> Result<u64, String> {
         let start = self.time_now();
         let result = self.vm.execute_on_coap_pkt(pkt);
-        self.results.borrow_mut().execution_time = self.time_now() - start;
+        let end = self.time_now();
+
+        self.results.borrow_mut().execution_time = end - start;
         return result;
     }
 }

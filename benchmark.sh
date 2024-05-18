2273bb264b7e830e40789685d3142bbf67705088
SzymonKubica <szymo.kubica@gmail.com>
2024-05-18 23:44:47 +0100

    Deleted old scripts and examples

--- benchmark.sh
+++
@@ -1,6 +0,0 @@
-for chunks in {0..7}
-do
- aiocoap-client -m POST 'coap://[fe80::a8e8:48ff:fee0:523c%wlan0]/benchmark' --payload "$chunks" | tee -a results.txt
- # Add a newline
- echo "" >> results.txt
-done
--- deploy-temp-hum-read.sh
+++
@@ -1,4 +0,0 @@
-RUST_LOG=DEBUG DOTENV=.env-nucleo ./tools/target/release/mibpf-tools --use-env deploy --bpf-source-file tools/tools/tests/test-sources/sensor-processing-update-thread.c -s 0 --binary-layout ExtendedHeader  --erase
-DOTENV=.env-nucleo RUST_LOG=DEBUG ./tools/target/release/mibpf-tools --use-env execute --suit-storage-slot 0 --execution-model LongRunning --binary-layout ExtendedHeader --helper-access-verification PreFlight --helper-access-list-source ExecuteRequest --helper-indices 1 48 49 50 17 19 82 52 96 97
-
-

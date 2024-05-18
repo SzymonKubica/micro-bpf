f9c78ee661fa000d6a9b61a1817b61081da8c018
SzymonKubica <szymo.kubica@gmail.com>
2024-05-18 12:19:34 +0100

    Adapted the weather station to use rodata correctly and run on the jit

--- bpf/helper-tests/hd44780_lcd.c
+++ bpf/helper-tests/hd44780_lcd.c
@@ -4,6 +4,10 @@
 #include <bpf/bpf_helpers.h>
 
 #define PERIOD_US (1000 * 1000)
+char MSG_1[] = "This is a test";
+char MSG_2[] = "Weather Station";
+char MSG_3[] = "  -- 2.0 --";
+
 int lcd_test(void *ctx)
 {
 
@@ -12,13 +16,13 @@ int lcd_test(void *ctx)
     uint32_t last_wakeup = bpf_ztimer_now();
 
     bpf_hd44780_clear(dev);
-    bpf_hd44780_print(dev, "This is a test");
+    bpf_hd44780_print(dev, MSG_1);
 
     bpf_ztimer_periodic_wakeup(&last_wakeup, PERIOD_US);
 
     bpf_hd44780_clear(dev);
-    bpf_hd44780_print(dev, "Weather Station");
+    bpf_hd44780_print(dev, MSG_2);
     bpf_hd44780_set_cursor(dev, 0, 1);
-    bpf_hd44780_print(dev, "  -- 2.0 --");
+    bpf_hd44780_print(dev, MSG_3);
     return 0;
 }

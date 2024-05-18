0b3dc5562c136276b621c5074fc5ffb4bd6fa676
SzymonKubica <szymo.kubica@gmail.com>
2024-05-18 23:45:24 +0100

    Moved the old deployment scripts to their separate directory

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,11 +1,6 @@
-board_ip=$1
-sum=0
-iterations=100
-for ((i=0; i<$iterations; i++));
+for chunks in {0..7}
 do
-  time=$(./execute-bpf.sh wlp3s0 $board_ip 0)
-  echo "Iteration $i: $time [us]"
-  sum=$(($sum + $time))
+ aiocoap-client -m POST 'coap://[fe80::a8e8:48ff:fee0:523c%wlan0]/benchmark' --payload "$chunks" | tee -a results.txt
+ # Add a newline
+ echo "" >> results.txt
 done
-
-echo "Average: $(($sum / $iterations)) [us]"

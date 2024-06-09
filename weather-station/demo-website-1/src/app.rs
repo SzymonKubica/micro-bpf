997296d2fde665076090f2e539d4ada316df9f62
SzymonKubica <szymo.kubica@gmail.com>
2024-06-09 22:33:12 +0100

    Added the simple admin tools website.

--- weather-station/demo-website-1/src/app.rs
+++ weather-station/demo-website-1/src/app.rs
@@ -1,7 +1,6 @@
 use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
-use leptos::logging::log;
 
 #[component]
 pub fn App() -> impl IntoView {
@@ -9,12 +8,10 @@ pub fn App() -> impl IntoView {
     provide_meta_context();
 
     view! {
-        // injects a stylesheet into the document <head>
-        // id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href="/pkg/demo-website-1.css"/>
 
         // sets the document title
-        <Title text="Welcome to Leptos"/>
+        <Title text="µBPF Admin Tools"/>
 
         // content for this welcome page
         <Router>
@@ -31,14 +28,10 @@ pub fn App() -> impl IntoView {
 /// Renders the home page of your application.
 #[component]
 fn HomePage() -> impl IntoView {
-    // Creates a reactive value to update the button
-    let (count, set_count) = create_signal(0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
-
     view! {
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
-        <ExecuteTrigger />
+        <h1>"µBPF Admin Tools"</h1>
+        <DeployForm/>
+        <ExecuteForm/>
     }
 }
 
@@ -59,50 +52,144 @@ fn NotFound() -> impl IntoView {
         resp.set_status(actix_web::http::StatusCode::NOT_FOUND);
     }
 
+    view! { <h1>"Not Found"</h1> }
+}
+
+#[component]
+fn ExecuteForm() -> impl IntoView {
+    let (slot, set_slot) = create_signal(0);
+    let (response, set_response) = create_signal("Loading".to_string());
+
+
+    let send_execution_request = create_action(|input: &usize|{
+        let storage_slot = input.to_owned();
+        async move {
+            let response = execute(storage_slot).await;
+            response.unwrap()
+        }
+    });
+
+
     view! {
-        <h1>"Not Found"</h1>
+        <p>"Execute Form"</p>
+        <p>"Enter the SUIT storage slot from where the program should be loaded: "</p>
+        <input
+            type="text"
+            on:input=move |ev| {
+                set_slot(event_target_value(&ev).parse::<i32>().unwrap());
+            }
+
+            prop:value=slot
+        />
+
+        <button on:click=move |_| {
+            let _ = send_execution_request.dispatch(slot.get() as usize);
+            set_response(send_execution_request.value().get().unwrap());
+        }>
+            "Send deploy request"
+        </button>
+        <p>"Response:"</p>
+        <p>{response}</p>
     }
 }
 
 #[component]
-fn ExecuteTrigger() -> impl IntoView {
-    let (count, set_count) = create_signal(0);
+fn DeployForm() -> impl IntoView {
+    let (name, set_name) = create_signal("file_name.c".to_string());
+    // Directory with all of the ebpf sources
+    let (directory, set_directory) = create_signal("bpf/".to_string());
+    let (slot, set_slot) = create_signal(0);
 
-    let async_data = create_resource(count, |_ | async move {
-        execute_request().await.unwrap()
+    let send_deploy_request = create_action(|input: &(String, usize)|{
+        let (source_file, storage_slot) = input.to_owned();
+        async move {
+            let _ = deploy(source_file, storage_slot).await;
+        }
     });
 
     view! {
-        <button
-            on:click=move |_| {
-                // on stable, this is set_count.set(3);
-                set_count.set(count.get() + 1);
+        <p>"Deploy Form"</p>
+        <p>"Enter the directory where the file is located: "</p>
+        <input
+            type="text"
+            on:input=move |ev| {
+                set_directory(event_target_value(&ev));
             }
-        >
-            "Response: "
-            // on stable, this is move || count.get();
-            {move || count.get()}
+
+            // the `prop:` syntax lets you update a DOM property,
+            // rather than an attribute.
+            prop:value=directory
+        />
+        <p>"Enter the file to be deployed: "</p>
+        <input
+            type="text"
+            on:input=move |ev| {
+                set_name(event_target_value(&ev));
+            }
+
+            // the `prop:` syntax lets you update a DOM property,
+            // rather than an attribute.
+            prop:value=name
+        />
+        <p>"Enter the target SUIT storage slot: "</p>
+        <input
+            type="text"
+            on:input=move |ev| {
+                set_slot(event_target_value(&ev).parse::<i32>().unwrap());
+            }
+
+            prop:value=slot
+        />
+
+        <button on:click=move |_| {
+            send_deploy_request.dispatch((format!("{}/{}", directory.get(), name.get()), slot.get() as usize));
+        }>
+            Send deploy request
         </button>
-        <p>
-            "Response: " {move || match async_data.get() {
-                Some(s) => s,
-                None => "Loading...".to_string(),
-            }}
-        </p>
     }
 }
 
-#[server(ExecuteRequest, "/execute")]
-pub async fn execute_request() -> Result<String, ServerFnError> {
+
+#[server(DeployRequest, "/deploy")]
+pub async fn deploy(source_file: String, storage_slot: usize) -> Result<(), ServerFnError> {
     use mibpf_common::*;
     use mibpf_tools::*;
+    let environment: Environment = load_env();
+
+    let deploy_response = deploy(
+     &source_file,
+     &environment.out_dir,
+     TargetVM::Rbpf,
+     BinaryFileLayout::RawObjectFile,
+     &environment.coap_root_dir,
+     storage_slot,
+     &environment.riot_instance_net_if,
+     &environment.riot_instance_ip,
+     &environment.host_net_if,
+     &environment.host_ip,
+     &environment.board_name,
+     Some(&environment.mibpf_root_dir),
+     vec![],
+     HelperAccessVerification::PreFlight,
+     HelperAccessListSource::ExecuteRequest,
+     true
+    ).await;
+
+    Ok(deploy_response.unwrap())
+}
+
+#[server(ExecuteRequest, "/execute")]
+pub async fn execute(storage_slot: usize) -> Result<String, ServerFnError> {
+    use mibpf_common::*;
+    use mibpf_tools::*;
+    let environment: Environment = load_env();
 
     let execution_response = execute(
-        "fe80::a8e8:48ff:fee0:523c",
+        &environment.riot_instance_ip,
         TargetVM::Rbpf,
         BinaryFileLayout::RawObjectFile,
-        0,
-        "enp82s0u2u1u2",
+        storage_slot,
+        &environment.host_net_if,
         ExecutionModel::ShortLived,
         HelperAccessVerification::PreFlight,
         HelperAccessListSource::ExecuteRequest,
